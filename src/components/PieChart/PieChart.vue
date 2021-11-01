29fe70bbc6157dd1f1f58bc881fa5f0b4e49497d
Maksim Ivanov <makes5712@gmail.com>
2021-11-02 02:08:41 +0300

    Добавляет основной функционал тТЗ

---
+++ src/components/PieChart/PieChart.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+import { Doughnut as PieChart, mixins } from"vue-chartjs";
+const { reactiveProp } = mixins
+console.log(reactiveProp)
+
+
+export default {
+  extends: PieChart,
+  mixins: [reactiveProp],
+  props: ['options'],
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
